a1b691ea00856160a37738d817f2b52da16e8a64
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2022-08-15 09:14:46 +0100

    card request application

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-
-// module.exports = nextConfig
-
-module.exports = {
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   env: {
     MYSQL_HOST: "192.168.207.8",
     MYSQL_PORT: "443",
-    MYSQL_DATABASE: { PRYME },
-    MYSQL_USER: { Pryme },
+    MYSQL_DATABASE: "PRYME ",
+    MYSQL_USER: "Pryme",
     MYSQL_PASSWORD: "Guff@VenusPro442!",
   },
 };
+
+module.exports = nextConfig;
